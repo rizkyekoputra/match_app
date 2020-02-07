88a497f085a60340c90ea1f328c28277f85c9ab9
Rizky Eko Putra <rizky.eko.putra@gmail.com>
2020-02-07 16:31:15 +0700

    Init

---
+++ Match App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Match App
+//
+//  Created by Rizky Eko Putra on 07/02/20.
+//  Copyright © 2020 Rizky Eko Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
